a43c02fcd1afcd7b4d84e9af76763a66487bd75c
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2023-10-15 16:23:45 +0700

    add animte on scroll #3

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const mobile_bars = document.querySelector(".mobile-bars");
 const mobile_nav_menu = document.querySelector(".mobile-nav-menu");
 const close_mobile_bars = document.querySelector(".close-mobile-bars");
 const nav_link_mobile = document.querySelectorAll(".nav-link-mobile");
+const animate_hidden = document.querySelectorAll(".animate-hidden");
+const animate_hidden_right = document.querySelectorAll(".animate-hidden-right");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("animate-show");
+        }
+    });
+})
+
+animate_hidden.forEach((el) => observer.observe(el));
+animate_hidden_right.forEach((el) => observer.observe(el));
 
 mobile_bars.addEventListener("click",() => {
     mobile_nav_menu.classList.remove("hide");
